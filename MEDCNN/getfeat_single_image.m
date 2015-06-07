3c66f4d2fcff07ed5febe5fd46fc46fb96cf078a
Liu Jiang <393444163@qq.com>
2015-06-07 10:36:10 +0800

    TRECVID MED 11, optimal model on CNN updated.

--- MEDCNN/getfeat_single_image.m
+++ MEDCNN/getfeat_single_image.m
@@ -1,13 +1,13 @@
 function [feat]=getfeat_single_image(im,net)
-  level=42;
+  level=20;
   im=imresize(im,[net.normalization.imageSize(1,1),net.normalization.imageSize(1,2)]);
   im=double(im)-net.normalization.averageImage;
   im=single(im);
   im=gpuArray(im);
   res=vl_simplenn(net,im);
   feat=gather(res(level).x(:)');
-  %size(feat)
-  %pause;
+ % size(feat)
+ % pause;
 end
 
 
